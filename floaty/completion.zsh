0c607cf1ab2b8f581bffe28a1e3f6df0f59d3ba0
Jeremy Barlow <jeremy.barlow@puppet.com>
2016-10-02 08:54:13 -0700

    Add completion functions for vmfloaty

---
+++ floaty/completion.zsh
@@ -0,0 +1,49 @@
+compctl -K _floaty floaty
+
+_floaty_commands() {
+  reply=(delete get help list modify query \
+      revert snapshot status summary token)
+}
+
+_floaty_oses() {
+  local oses="$(floaty list)"
+  reply=("${(ps:\n:)oses}")
+}
+
+_floaty_vms() {
+  local vms="$(floaty list --active | grep "-" | cut -f2 -d' ')"
+  reply=("${(ps:\n:)vms}")
+}
+
+_floaty() {
+  local word words
+  read -cA words
+  word="${words[2]}"
+
+  case ${#words} in
+    2)
+      _floaty_commands ;;
+    3)
+      case $word in
+        delete|modify|query|revert|snapshot)
+          _floaty_vms ;;
+        get)
+          _floaty_oses ;;
+        help)
+          _floaty_commands ;;
+        list)
+          reply=(--active) ;;
+      esac ;;
+    *)
+      case $word in
+        delete|snapshot)
+          _floaty_vms ;;
+        get)
+          _floaty_oses ;;
+        modify)
+          reply=(--lifetime --disk --tags) ;;
+        revert)
+          reply=(--snapshot) ;;
+      esac ;;
+  esac
+}
