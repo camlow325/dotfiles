ecdad15aefeed386f770c762a0b1389ee18c7951
Jeremy Barlow <niobrara@gmx.com>
2021-04-06 21:25:14 -0600

    Add awsume alias

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -2,4 +2,12 @@ if (( $+commands[pyenv] ))
 then
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
+
+  if (( $+commands[awsume] ))
+  then
+    alias awsume=". $(pyenv which awsume)"
+  fi
+elif (( $+commands[awsume] ))
+then
+  alias awsume=". awsume"
 fi
