826eba032cd8be3d038a475a920bc77dd7f1d408
Jeremy Barlow <niobrara@gmx.com>
2021-04-17 22:54:57 -0600

    Move pyenv and rbenv init into system _path.zsh for path precedence

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,26 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Initialize `pyenv` and `rbenv` after setting up system paths so that their
+# executables can take precedence.
+if (( $+commands[pyenv] ))
+then
+  eval "$(pyenv init -)"
+
+  if pyenv commands | grep -q ^virtualenv-init$; then
+    eval "$(pyenv virtualenv-init -)"
+  fi
+
+  if (( $+commands[awsume] ))
+  then
+    alias awsume=". $(pyenv which awsume)"
+  fi
+elif (( $+commands[awsume] ))
+then
+  alias awsume=". awsume"
+fi
+
+if (( $+commands[rbenv] ))
+then
+    eval "$(rbenv init -)"
+fi
