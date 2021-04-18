21e1ff458e2f437d0350c21a3731485041447458
Jeremy Barlow <niobrara@gmx.com>
2021-04-18 10:56:54 -0600

    Colorize ls output for linux

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,5 +6,16 @@ then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias la="gls -A --color"
+elif test "$(uname)" = "Darwin"
+then
+  alias ls="ls -F"
+  alias l="ls -lAh"
+  alias ll="ls -l"
+  alias la="ls -A"
+else
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la="ls -A --color"
 fi
